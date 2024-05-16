48b069e2631a78ef3d526ca9dc00712808700160
kedar <kekniskedar@gmail.com>
2024-05-16 16:11:15 +0530

    05/16/24 16:11:15 — master: . Signed-off-by: kedar <kekniskedar@gmail.com>

---
+++ NoteBook/max_of_two_numbers.py
@@ -0,0 +1,5 @@
+max_len = 4
+current_len = 5
+max_len = max(max_len, current_len)
+
+print(max_len)
--- test/test_10.py
+++ test/test_10.py
@@ -1,16 +1,27 @@
-import re
+def max_alternate_subsequence(arr):
+    if not arr:
+        return 0
 
+    # Initialize variables to keep track of the length of the alternating subsequence
+    max_len = 0  # Maximum length of alternating subsequence
+    current_len = 1  # Length of current alternating subsequence
 
-def extract_names(formula_str):
-    # Define regex pattern for extracting names
-    pattern = r'[a-zA-Z\s]+'
-    # Find all names in the formula using regex pattern
-    formula_list = re.findall(pattern, formula_str)
+    # Iterate through the array starting from the second element
+    for i in range(1, len(arr)):
+        if arr[i] != arr[i - 1]:
+            # If the current element is different from the previous one, it's part of the alternating subsequence
+            current_len += 1
+        else:
+            # If the current element is the same as the previous one, the alternating subsequence ends here
+            max_len = max(max_len, current_len)  # Update max_len if necessary
+            current_len = 1  # Reset current_len
 
-    return formula_list
+    # Update max_len in case the last element extends the alternating subsequence
+    max_len = max(max_len, current_len)
+
+    return max_len
 
 
 # Example usage:
-formula = "Premium Home Buildings * (Premium Home Contents + Premium Home Valuables) + Premium Domestic Animals + Premium Prop Damage - Premium Frozen Food + Premium Livestock Non Disease + Premium Livestock Disease / 20"
-names = extract_names(formula)
-print(names)
+arr = [1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1]
+print(max_alternate_subsequence(arr))
